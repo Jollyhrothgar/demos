d17bd2d85386f2f6fbb0c561ec950d1208e54442
Mike Beaumier <michael.beaumier@gmail.com>
2017-01-20 15:41:12 -0800

    testing webhook

--- mike_module/mike_module.py
+++ mike_module/mike_module.py
@@ -7,6 +7,6 @@ def nice_table():
     Prints a nice table
     """
     nice_table = {
-            "Nice Features About Mike":["Nice features about Mike","he is nice","he built you this nice framework","he is devilishly handsome"]
+            "Nice Features About Mike":["Nice features about Mike","he is nice","he built you this nice framework","he is devilishly handsome","so practical and versitile!"]
     }
     return nice_table
