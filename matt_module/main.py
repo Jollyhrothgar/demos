c8816f40d6c2bb943315b5ecb810fff74c0602cf
Matthew Pawlicki <matthew.pawlicki@daimler.com>
2017-01-20 17:15:09 -0800

    Summing combos math

--- matt_module/main.py
+++ matt_module/main.py
@@ -1,5 +1,9 @@
 import mlpux
+import math
 
 @mlpux.Demo()
-def get_square(x):
-    return x**2
+def sum_combinations(n):
+    sum = 0
+    for k in range(n+1):
+        sum += math.factorial(n) / (math.factorial(k) * math.factorial(n-k))
+    return sum
